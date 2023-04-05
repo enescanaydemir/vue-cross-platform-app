003e00773109c09b6eab0f571ada4419b0385e8a
Enes Can Aydemir <enescan_aydemir@hotmail.com>
2023-04-06 01:49:41 +0300

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-page padding>
+    <input
+    v-model="message"
+    @keyup.esc="clearMessage"
+    @keyup.enter="alertMessage"
+    />
+    <button @click="clearMessage">Clear</button>
+    <h5>{{ message }}</h5>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "in to the game",
+      errorMessage: "yapacağın işi s2m"
+    }
+  },
+  methods: {
+    clearMessage() {
+      this.message = ''
+    },
+    alertMessage() {
+      alert(this.errorMessage)
+    }
+  },
+}
+</script>
